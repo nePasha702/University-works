7bb77e3ffd4e8c9bc4536ff3b9882ad0d4513e16
nePasha702 <1982mitrich@gmail.com>
2025-12-03 01:18:11 +0300

    Create main.h

---
+++ sem3/PPOIS/Multiset/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "include/Multiset.h"
+#include <iostream>
+
+// Прототипы функций меню
+void displayMenu();
+void runMenu();
+void clearInputBuffer();
+
+#endif
