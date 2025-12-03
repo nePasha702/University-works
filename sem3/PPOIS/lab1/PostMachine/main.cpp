ddd8f4288d1fc4b7a6afc5f4d098711ab577c15e
nePasha702 <1982mitrich@gmail.com>
2025-12-03 22:27:27 +0300

    Update main.cpp

--- sem3/PPOIS/lab1/PostMachine/main.cpp
+++ sem3/PPOIS/lab1/PostMachine/main.cpp
@@ -1,1 +1,10 @@
+#include "include/Menu.h"
+#include <iostream>
 
+int main() {
+    PostMachine machine;
+    std::cout << "Задание: Машина Поста\n";
+    Menu::handleUserInput(machine);
+    std::cout << "Программа завершена\n";
+    return 0;
+}
