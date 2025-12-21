df29a250ec2746f5a84123e3ff6c0d11b602d23b
nePasha702 <1982mitrich@gmail.com>
2025-12-21 22:02:09 +0300

    Remove comment from deserialize function

--- sem3/PPOIS/lab2/DocumentFlow/modules/core/src/Deserializer.cpp
+++ sem3/PPOIS/lab2/DocumentFlow/modules/core/src/Deserializer.cpp
@@ -4,6 +4,5 @@ Deserializer::Deserializer()
     : format("JSON"), strictMode(true), maxDepth(5) {}
 
 std::string Deserializer::deserialize(std::string data) {
-    // Упрощенная реализация
     return "Данные из " + format + ": " + data;
 }
